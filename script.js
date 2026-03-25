08a535f410ff859ef32bf235ff83785d9f0ebf52
James230kk <maaaazen3ff@gmail.com>
2026-03-25 23:15:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,10 @@ const video = document.getElementById("video");
 const canvas = document.getElementById("canvas");
 const snapBtn = document.getElementById("snap");
 
-// Ask for camera access
 navigator.mediaDevices.getUserMedia({ video: true })
-  .then(stream => {
-    video.srcObject = stream;
-  })
-  .catch(err => {
-    alert("Camera access blocked");
-    console.error(err);
-  });
+  .then(stream => video.srcObject = stream)
+  .catch(err => console.error("Camera blocked:", err));
 
-// Take photo and send to server
 snapBtn.onclick = () => {
   canvas.width = video.videoWidth;
   canvas.height = video.videoHeight;
@@ -28,6 +21,6 @@ snapBtn.onclick = () => {
     body: JSON.stringify({ image: imageData })
   })
   .then(res => res.text())
-  .then(msg => console.log(msg))
+  .then(msg => console.log("Server:", msg))
   .catch(err => console.error(err));
 };
